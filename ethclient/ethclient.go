d63078f2ee273337fe028fe550c07973661bd75a
tudyzhb <tudyzhb@gmail.com>
2018-10-10 11:42:11 +0800

    chore: mask uncles error

--- ethclient/ethclient.go
+++ ethclient/ethclient.go
@@ -117,26 +117,27 @@ func (ec *Client) getBlock(ctx context.Context, method string, args ...interface
 	// Load uncles because they are not included in the block response.
 	var uncles []*types.Header
 	if len(body.UncleHashes) > 0 {
-		uncles = make([]*types.Header, len(body.UncleHashes))
-		reqs := make([]rpc.BatchElem, len(body.UncleHashes))
-		for i := range reqs {
-			reqs[i] = rpc.BatchElem{
-				Method: "eth_getUncleByBlockHashAndIndex",
-				Args:   []interface{}{body.Hash, hexutil.EncodeUint64(uint64(i))},
-				Result: &uncles[i],
-			}
-		}
-		if err := ec.c.BatchCallContext(ctx, reqs); err != nil {
-			return nil, err
-		}
-		for i := range reqs {
-			if reqs[i].Error != nil {
-				return nil, reqs[i].Error
-			}
-			if uncles[i] == nil {
-				return nil, fmt.Errorf("got null header for uncle %d of block %x", i, body.Hash[:])
-			}
-		}
+		// FIXME: 2018.05.18 tudy: ignore uncles in bytebank project.
+		//uncles = make([]*types.Header, len(body.UncleHashes))
+		//reqs := make([]rpc.BatchElem, len(body.UncleHashes))
+		//for i := range reqs {
+		//	reqs[i] = rpc.BatchElem{
+		//		Method: "eth_getUncleByBlockHashAndIndex",
+		//		Args:   []interface{}{body.Hash, hexutil.EncodeUint64(uint64(i))},
+		//		Result: &uncles[i],
+		//	}
+		//}
+		//if err := ec.c.BatchCallContext(ctx, reqs); err != nil {
+		//	return nil, err
+		//}
+		//for i := range reqs {
+		//	if reqs[i].Error != nil {
+		//		return nil, reqs[i].Error
+		//	}
+		//	if uncles[i] == nil {
+		//		return nil, fmt.Errorf("got null header for uncle %d of block %x", i, body.Hash[:])
+		//	}
+		//}
 	}
 	// Fill the sender cache of transactions in the block.
 	txs := make([]*types.Transaction, len(body.Transactions))
